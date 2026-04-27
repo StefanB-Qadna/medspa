9287e72cc0ab68a922d3b5794c20ab6e92ec9309
Ovidiu <ovidiu.c@qadna.co>
2026-04-27 10:28:10 +0300

    restructured and removed archive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Jost } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
+import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
 import { SmoothScrollProvider } from "@/components/SmoothScroll";
 import { BoulevardWidget } from "@/components/BoulevardWidget";
 
